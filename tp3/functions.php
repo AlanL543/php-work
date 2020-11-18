355f0443dd6ccaa72df1caf24eb7adbab05a65e5
Alan Leveque <aleveque@sio.local>
2020-11-18 09:50:11 +0100

    addfiles

---
+++ tp3/functions.php
@@ -0,0 +1,24 @@
+<?php
+function getHeader($startSession,$title) {
+  if($startSession) {
+    session_start();
+  }
+}
+ ?>
+ <!DOCTYPE html>
+ <html>
+ <head>
+   <meta charset="utf-8">
+   <title><?=$title?></title>
+ </head>
+ <body>
+<?php
+function getFooter() {
+
+}
+?>
+</body>
+</html>
+<?php
+
+ ?>
