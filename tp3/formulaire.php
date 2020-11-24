f968279dfd6992f3743029e9f761b7521ca17fc0
Alan Lévêque <alan.leveque@sts-sio-caen.info>
2020-11-24 12:27:56 +0100

    addfiles

---
+++ tp3/formulaire.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title><?=$title?></title>
+</head>
+<body>
+<label>Nom : </label>
+<input type="text" name="nom" placeholder="Saisissez votre nom">
+</body>
+</html>
